cf0c8955fd60a044d8285eb09e1980e8521b4224
mdarmstr <mdarmstr@ualberta.ca>
2023-07-27 04:18:11 +0200

    made some progress on the statistical filtering

--- infft.py
+++ infft.py
@@ -4,6 +4,9 @@ from nfft import nfft as nfft
 from nfft import nfft_adjoint as adjoint
 import scipy.optimize
 import matplotlib.pyplot as plt
+import imageio
+
+from scipy.stats import chi2
 
 def change_last_true_to_false(arr):
     arr = np.asarray(arr)
@@ -21,46 +24,66 @@ def fjr(x,N):
 def nat_norm(f,w):
     return np.sum(abs(f)**2 / w)
 
-def infft(x, y, N, w=1, f0 = None, maxiter = 10, L=10, tol = 1e-16, is_verbose = True):
+def create_frame(iter):
+    plt.savefig(f'./img/img_{iter}.png',transparent=False,facecolor='white')
+    plt.close()
+
+def infft(x, y, N, w=1, f0 = None, maxiter = 10, L=10, tol = 1e-16, is_verbose = True, create_gif=False):
     
     res = []
+    nnz = x.shape[0]
 
     if f0 is None:
         f = np.zeros(N,dtype=np.complex128)
     else:
         f = f0.copy()
     
-    r = y - nfft(x, f) / N
+    r = y - nfft(x, f) / nnz
     p = adjoint(x,r,N) 
 
     rnm1 = np.linalg.norm(r) ** 2
     rnm2 = 0
-    nat1 = np.linalg.norm(f*w)
-    nat2 = 0
     iter = 0
 
-    while np.abs(nat1 - nat2) > tol and iter < maxiter:
+    while np.abs(rnm1 - rnm2) / rnm1 > tol and iter < maxiter:
         if iter > 0:
-            nat1 = nat2
+            rnm1 = rnm2
         
         pnm = np.linalg.norm(p * w) ** 2
         alf = rnm1 / pnm
         f += alf * w * p
-        r = y - nfft(x, f) / N
+        r = y - nfft(x, f) / nnz
         rnm2 = np.linalg.norm(r) ** 2
         bta = rnm2 / rnm1
         p = bta * p + adjoint(x,r, N)
-        nat2 = np.linalg.norm((f - f0) * w)
+        #nat2 = np.linalg.norm((f - f0) * w)
 
-        res.append(nat2)
+        res.append(rnm2)
 
         if is_verbose == True: 
-            print(iter, ' {:.5E}'.format(nat2),' {:.5E}'.format(nat1 - nat2))
+            print(iter, ' {:.5E}'.format(rnm2),' {:.5E}'.format((rnm1 - rnm2)/rnm1))
+        
+        if create_gif == True:
+            plt.plot(r,alpha=0.25)
+            plt.plot(nfft(x,f) / len(x), alpha = 0.75)
+            plt.ylim((min(y),max(y)))
+            plt.title(f'iNFFT Iteration {iter}')
+            create_frame(iter)
 
         iter += 1
 
+    if create_gif == True:
+            frames = []
+
+            for t in range(iter):
+                image = imageio.v2.imread(f'./img/img_{t}.png')
+                frames.append(image)
+        
+            imageio.mimsave('./output.gif', frames, duration = 0.01)
+
     return f, r, res
 
+## This part is just reading the data
 df = pd.read_csv('T.Suelo.csv')
 Ln = df.shape[0]
 smplR = 1800
@@ -71,29 +94,62 @@ rec_mat = np.zeros_like(data_raw,dtype="float64")
 mni = np.zeros((df.shape[1]-1,1))
 #data_raw, Ln = ensure_even(data_raw, Ln)
 t = np.linspace(-0.5,0.5,Ln,endpoint=False)
+mn = []
+N = 48
+inverse_mat = np.zeros((N,df.shape[1]-1),dtype="complex128")
 
-y = data_raw[:,0]
-idx = y != -9999
+for ii in range(df.shape[1]-1):
+    idx = data_raw[:,ii] != -9999
+    if sum(idx) % 2 != 0:
+        idx = change_last_true_to_false(idx)
+        Ln = sum(idx)
+    else:
+        Ln = sum(idx)
 
-idx = change_last_true_to_false(idx)
-Ln = sum(idx)
-x = t[idx]
+    x = t[idx]
+    mn.append(np.mean(data_raw[idx,ii]))
+    
+    f0 = adjoint(x,data_raw[idx,ii] - mn[ii], N)
 
-N = 256
-w = fjr(np.linspace(-N/2, N/2,N,endpoint=False),N)
+    h_hat, r, res = infft(x, data_raw[idx,ii] - mn[ii], N, f0=f0, w = 1, maxiter = 50, tol = 1e-7, create_gif=False)
+        
+    inverse_mat[:,ii] = h_hat
+    print(ii)
 
-mn = np.mean(y[idx])
+print("inverse NFFTs complete")
+k = 5
+U, S, V = np.linalg.svd(inverse_mat)
+rec_inv = (U[:,:k]*S[:k]) @ V[:,:k].T
 
-f0 = adjoint(x,y[idx], N)
+# Reconstructing the data, measuring the residuals
+for ii in range(df.shape[1]-1):
+    idx = data_raw[:,ii] != -9999
+    
+    rec_mat[:,ii] = np.real(nfft(t, rec_inv[:,ii]) / t.shape[0] + mn[ii])
+    residue_mat[idx,ii] = rec_mat[idx,ii] - data_raw[idx,ii]
+    print(ii)
 
-f, r, res = infft(x, y[idx], N, w = w, f0=f0, maxiter = 15, tol = 1e-8) 
+np.save("inverse_mat.npy", inverse_mat)
+np.save("inverse_res.npy",residue_mat)
+np.save("data_raw.npy",data_raw)
+np.save("rec_mat.npy",rec_mat)
+print("finished")
 
-#plt.plot(nfft(x,f) / Ln + mn)
-plt.plot(x,y[idx])
-#plt.plot(res)
-plt.plot(t, nfft(t,f) / Ln)
-#plt.plot(f)
+# y = data_raw[:,0]
+# idx = y != -9999
 
-plt.show()
+# idx = change_last_true_to_false(idx)
+# #Ln = sum(idx)
+# x = t[idx]
+
+# N = 48
+# w = fjr(np.linspace(-N/2, N/2,N,endpoint=False),N)
+
+# mn = np.mean(y[idx])
+
+# f0 = adjoint(x,y[idx]-mn, N)
+
+# f, r, res = infft(x, y[idx] - mn, N, w = 1, f0=f0, maxiter = 1500, tol = 5e-8, create_gif=True) 
+
 
 
