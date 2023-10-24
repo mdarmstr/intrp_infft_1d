11b9a6ba6489e0b99e21b5d89eb3685063d204d1
Michael D. Sorochan Armstrong <45431766+mdarmstr@users.noreply.github.com>
2023-10-24 18:27:33 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="intrp_infft_1d",
+      version="0.0.1",
+      description="interpolative fast fourier transform for non-equispaced data",
+      license='GNU v3.0',
+      author="Michael Sorochan Armstrong",
+      author_email="mdarmstr(at)go.ugr.es",
+      url='https://github.com/mdarmstr/intrp_infft_1d'
+     )
