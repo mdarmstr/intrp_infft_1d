452c453efd69db63eec0bd2f24b70513991b8756
Michael D. Sorochan Armstrong <45431766+mdarmstr@users.noreply.github.com>
2023-10-24 20:30:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,10 @@ setup(name="intrp_infft_1d",
       author="Michael Sorochan Armstrong",
       author_email="mdarmstr(at)go.ugr.es",
       url='https://github.com/mdarmstr/intrp_infft_1d',
-      packages=['intrp_infft_1d']
+      packages=['intrp_infft_1d'],
+      install_requires=[
+            'nfft',
+            'scipy',
+            'numpy',
+      ]
      )
